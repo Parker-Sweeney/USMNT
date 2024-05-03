a079b779d4f6a804800b277d2e286eff8a34065c
Parker Sweeney <77077612+Parker-Sweeney@users.noreply.github.com>
2024-05-03 17:37:13 -0500

    Update player_value_cleaning.R

--- player_value_cleaning.R
+++ player_value_cleaning.R
@@ -6,26 +6,24 @@ library(readr)
 # Read data from CSV file
 player_valuations <- read_csv("player_valuations.csv")
 
-# Define a function to adjust and format the date
+# Function To Format Date
 adjust_and_format_date <- function(date) {
-  # Convert to Date object if it's not already
+  # Convert to Date type if not already
   if (!inherits(date, "Date")) {
     date <- as.Date(date, format = "%Y-%m-%d")
   }
   
-  # Check if the date conversion was successful
+  # Make sure conversion happened
   if (is.na(date)) {
-    return(NA)  # Return NA if the date conversion fails
+    return(NA)
   }
   
-  # Determine the formatted date based on the month
+  # Format differently depending on month of year (better season allocation)
   if (month(date) < 8) {
-    # Subtract one year
     year_adjusted <- year(date) - 1
     # Format as year to year (previous to current)
     formatted_date <- paste0(year_adjusted, "/", format(date, "%y"))
   } else {
-    # Add one year
     year_adjusted <- year(date) + 1
     # Format as year to year (current to next)
     formatted_date <- paste0(year(date), "/", format(as.Date(paste0(year_adjusted, "-01-01")), "%y"))
